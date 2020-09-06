63541fd668153a42cb4f0e068014a0f1e40883e3
unknown <vladislav.batechko90@gmail.com>
2020-09-06 18:44:39 +0300

    second commit

--- js/cours.js
+++ js/cours.js
@@ -1,13 +1,25 @@
-fetch(`https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5`)
-    .then(response => response.json(0))
-    .then(data => {
-        console.log(data)
-            // let usdValue = data['name'];
-            // let rubValue = data['main']['temp'];
-        let euroValue = data['buy'];
+let usd = document.querySelector('.usd');
+let rub = document.querySelector('.rub');
+let euro = document.querySelector('.euro');
 
-        // usd.innerHTML = usdValue;
-        // rub.innerHTML = rubValue;
+function timeout(ms, promise) {
+    return new Promise(function(resolve, reject) {
+        setInterval(function() {
+            reject(new Error("timeout"))
+        }, ms)
+        promise.then(resolve, reject)
+    })
+}
+
+timeout(3600000, fetch(`https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json`)
+    .then(response => response.json())
+    .then(data => {
+        let usdValue = data[26].rate;
+        let rubValue = data[18].rate;
+        let euroValue = data[33].rate;
+
+        usd.innerHTML = usdValue;
+        rub.innerHTML = rubValue;
         euro.innerHTML = euroValue;
     })
-    .catch(err => console.error('smt Error'))
+    .catch(err => console.error('API timeout error')))
